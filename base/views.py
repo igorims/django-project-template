49dc7dbaf7d4c304f3ea4395687beecd14269376
igorsimb <gorets@gmx.com>
2021-09-08 02:08:44 +0300

    Initial Commit

---
+++ base/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+def index(request):
+    return render(request, 'base/index.html')
