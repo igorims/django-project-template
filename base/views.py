503e80a2a7af56eb12dc2d686f8d8be79e239141
igorsimb <gorets@gmx.com>
2021-10-01 12:26:41 +0300

    Changed Index to class based view

--- base/views.py
+++ base/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
 
-def index(request):
-    return render(request, 'base/index.html')
+class IndexView(TemplateView):
+    template_name = 'base/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        return context
